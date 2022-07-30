ec127e6634e4552a9681a606639cde7eabe8eb2a
cyb3rgh05t <flammang.yves@gmail.com>
2022-07-30 20:48:45 +0200

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -8,34 +8,34 @@ module.exports = class Logger {
 		switch (type) {
 	
 		case "log": {
-			return console.log(`[${chalk.gray(date)}]: [${chalk.black.bgBlue(type.toUpperCase())}] ${content}`);
+			return console.log(`[${chalk.gray(date)}]: [${chalk.magenta(type.toUpperCase())}] ${content}`);
 		}
 		case "warn": {
-			return console.log(`[${chalk.gray(date)}]: [${chalk.black.bgYellow(type.toUpperCase())}] ${content}`);
+			return console.log(`[${chalk.gray(date)}]: [${chalk.yellow(type.toUpperCase())}] ${content}`);
 		}
 		case "error": {
-			return console.log(`[${chalk.gray(date)}]: [${chalk.black.bgRed(type.toUpperCase())}] ${content}`);
+			return console.log(`[${chalk.gray(date)}]: [${chalk.red(type.toUpperCase())}] ${content}`);
 		}
 		case "debug": {
-			return console.log(`[${chalk.gray(date)}]: [${chalk.black.bgGreen(type.toUpperCase())}] ${content}`);
+			return console.log(`[${chalk.gray(date)}]: [${chalk.blue(type.toUpperCase())}] ${content}`);
 		}
 		case "cmd": {
-			return console.log(`[${chalk.gray(date)}]: [${chalk.black.bgWhite(type.toUpperCase())}] ${content}`);
+			return console.log(`[${chalk.gray(date)}]: [${chalk.green(type.toUpperCase())}] ${content}`);
 		}
 		case "event": {
-			return console.log(`[${chalk.gray(date)}]: [${chalk.black.bgWhite(type.toUpperCase())}] ${content}`);
+			return console.log(`[${chalk.gray(date)}]: [${chalk.green(type.toUpperCase())}] ${content}`);
 		}
 		case "buttons": {
-			return console.log(`[${chalk.gray(date)}]: [${chalk.black.bgWhite(type.toUpperCase())}] ${content}`);
+			return console.log(`[${chalk.gray(date)}]: [${chalk.green(type.toUpperCase())}] ${content}`);
 		}
 		case "prefix": {
-			return console.log(`[${chalk.gray(date)}]: [${chalk.black.bgWhite(type.toUpperCase())}] ${content}`);
+			return console.log(`[${chalk.gray(date)}]: [${chalk.green(type.toUpperCase())}] ${content}`);
 		}
 		case "modals": {
-			return console.log(`[${chalk.gray(date)}]: [${chalk.black.bgWhite(type.toUpperCase())}] ${content}`);
+			return console.log(`[${chalk.gray(date)}]: [${chalk.green(type.toUpperCase())}] ${content}`);
 		}
 		case "ready": {
-			return console.log(`[${chalk.gray(date)}]: [${chalk.black.bgBlueBright(type.toUpperCase())}] ${content}`);
+			return console.log(`[${chalk.gray(date)}]: [${chalk.green(type.toUpperCase())}] ${content}`);
 		} 
 		default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd, buttons, modals or error.");
 		}
