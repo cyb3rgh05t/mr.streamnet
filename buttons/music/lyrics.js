b61467441581d4518d85fc1f2e8c45e69090b54c
cyb3rgh05t <flammang.yves@gmail.com>
2022-08-17 20:57:01 +0200

    update lyrics

--- buttons/music/lyrics.js
+++ buttons/music/lyrics.js
@@ -9,7 +9,7 @@ const {
 } = require("../../src/config/config.json");
 const util = require("../../utils/util");
 const genius = require("genius-lyrics");
-const gClient = new genius.Client(lyricsKey);
+const gClient = new genius.Client("top-secret-optional-key");
 const DB = require("../../src/databases/musicDB");
 
 
