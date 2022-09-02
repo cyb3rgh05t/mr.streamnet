dc8c1d845ed2080f41901f69b4e85616ea66671c
cyb3rgh05t <flammang.yves@gmail.com>
2022-09-02 22:27:09 +0200

    permission fix

--- slashCommands/System/reboot.js
+++ slashCommands/System/reboot.js
@@ -11,7 +11,7 @@ module.exports = {
     name: "reboot",
     description: "Reboot the bot (DANGEROUS)",
     usage: "/reboot",
-    permissions: "ADMINISTRATOR",
+    permission: "ADMINISTRATOR",
     /**
      * 
      * @param {Client} client 
