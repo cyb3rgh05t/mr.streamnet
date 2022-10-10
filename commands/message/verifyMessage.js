58e6e2237eebc789c36c1dd5f3c647865f14da4e
cyb3rgh05t <flammang.yves@gmail.com>
2022-10-10 20:30:38 +0200

    Update verifyMessage.js

--- commands/message/verifyMessage.js
+++ commands/message/verifyMessage.js
@@ -14,7 +14,7 @@ module.exports = {
   run: async (client, message, args) => {
     try {
       message.channel.send({
-        content: `➡️ ... warte bis du vom Admin verifiziert wurdest ✅\n\n➡️ bitte erfülle das CAPTCHA in deinen DM's ...`
+        content: `➡️ ... warte bis du vom Admin verifiziert wurdest ✅\n\n➡️ bitte erfülle das CAPTCHA in deinen DM's ...\n(Bitte stelle sicher, dass Direktnachrichten aktiviert sind! Du kannst deine Direktnachrichten wieder deaktivieren, sobald Sie das Captcha abgeschlossen haben.)`
       });
     } catch (error) {
       message.channel.send("Some Error Occured");
