0cf43ce3fabf0c13aa8e09fe053f4f8420a85b79
cyb3rgh05t <flammang.yves@gmail.com>
2022-10-13 17:40:12 +0200

    Update verifyMessage.js

--- commands/message/verifyMessage.js
+++ commands/message/verifyMessage.js
@@ -13,8 +13,10 @@ module.exports = {
    */
   run: async (client, message, args) => {
     try {
+
       message.channel.send({
-        content: `➡️ ... warte bis du vom Admin verifiziert wurdest ✅\n\n➡️ bitte erfülle das CAPTCHA in deinen DM's ...\n(Bitte stelle sicher, dass Direktnachrichten aktiviert sind! Du kannst deine Direktnachrichten wieder deaktivieren, sobald Sie das Captcha abgeschlossen haben.)`
+        content: `➡️ ... warte bis du vom Admin verifiziert wurdest ✅\n\n➡️ bitte erfülle das CAPTCHA in deinen DM's ...\n(❌ Bitte stelle sicher, dass Direktnachrichten aktiviert sind! Du kannst deine Direktnachrichten wieder deaktivieren, sobald du das Captcha abgeschlossen hast.)`,
+        files: ["https://github.com/cyb3rgh05t/images/blob/master/StreamNet/Different%20App%20Logos/discord.png?raw=true"]
       });
     } catch (error) {
       message.channel.send("Some Error Occured");
