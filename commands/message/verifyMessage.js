06a0b21614efd8a2701b79d0a9db43fb3a7e4c1b
cyb3rgh05t <flammang.yves@gmail.com>
2024-04-16 21:03:00 +0200

    Update verifyMessage.js

--- commands/message/verifyMessage.js
+++ commands/message/verifyMessage.js
@@ -1,5 +1,4 @@
-const { Client, Message, MessageAttachment, MessageEmbed } = require("discord.js");
-const fs = require('fs');
+const { Client, Message, MessageAttachment } = require("discord.js");
 
 module.exports = {
   name: "verify-message",
@@ -14,17 +13,17 @@ module.exports = {
    */
   run: async (client, message, args) => {
     try {
+
       const imagePath = './src/images/discord.png'; // Replace this with the path to your local image file
       const file = new MessageAttachment(imagePath);
-      const embed = new MessageEmbed()
-        .setDescription(`➡️ ... warte bis du vom Admin verifiziert wurdest <:approved:995615632961847406>\n\n➡️ bitte erfülle das CAPTCHA in deinen DM's ...\n(<:rejected:995614671128244224> Bitte stelle sicher, dass Direktnachrichten aktiviert sind! Du kannst deine Direktnachrichten wieder deaktivieren, sobald du das Captcha abgeschlossen hast.)`)
-        .attachFiles(file)
-        .setImage('attachment://image.png');
 
-      await message.channel.send({ embeds: [embed] });
+      await message.channel.send({
+        content: `➡️ ... warte bis du vom Admin verifiziert wurdest <:approved:995615632961847406>\n\n➡️ bitte erfülle das CAPTCHA in deinen DM's ...\n(<:rejected:995614671128244224> Bitte stelle sicher, dass Direktnachrichten aktiviert sind! Du kannst deine Direktnachrichten wieder deaktivieren, sobald du das Captcha abgeschlossen hast.)`,
+        files: [file]
+      });
     } catch (error) {
       message.channel.send("Some Error Occured");
-      client.logger.log(error, "error");
+      client.logger.log(error, "error")
     }
   }
-};
+}
